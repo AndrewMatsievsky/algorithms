6666c2e7ef6693bb770e0470b28c840cf1345147
Andrii Matsiievskyi <strymok89@gmail.com>
2023-01-26 23:35:38 +0400

    Test commit gui

--- sorting/bubble.ts
+++ sorting/bubble.ts
@@ -1,10 +1,10 @@
 function bubbleSort(arr: number[], n: number): number[] {
-    for (let i=0; i < n-1;i++ ) {
-        for (let j = 0; j < n-1-i; j++) {
-            if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        }
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
-    return arr;
+  }
+  return arr;
 }
 
 const arr = [5, 4, 3, 2, 1, 2, 3, 5, 1, 1, 3];
--- sorting/merge.ts
+++ sorting/merge.ts
@@ -1,31 +1,31 @@
 function mergeSort(arr: number[]): number[] {
-    if(arr.length === 1) {
-        return arr;
-    }
+  if (arr.length === 1) {
+    return arr;
+  }
 
-    const pivotIndex: number = Math.floor(arr.length / 2);
+  const pivotIndex: number = Math.floor(arr.length / 2);
 
-    const leftPart:  number[] = arr.slice(0, pivotIndex);
-    const rightPart: number[] = arr.slice(pivotIndex);
+  const leftPart: number[] = arr.slice(0, pivotIndex);
+  const rightPart: number[] = arr.slice(pivotIndex);
 
-    return merge(mergeSort(leftPart), mergeSort(rightPart))
+  return merge(mergeSort(leftPart), mergeSort(rightPart));
 }
 
 function merge(left, right) {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  const result = [];
 
-    while(leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            result.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            result.push(right[rightIndex]);
-            rightIndex++;
-        }
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
     }
-    return result.concat((left.slice(leftIndex))).concat(right.slice(rightIndex));
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 console.log(mergeSort([2, 5, 1, 3, 4, 1, 0]));
